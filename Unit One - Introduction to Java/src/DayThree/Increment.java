06c5be1437449eef449d58a5274b655d3da76ec7
Ross Conrad <rcontrad@bayviewglen.ca>
2023-09-26 08:17:54 -0400

    SEpt 26

---
+++ Unit One - Introduction to Java/src/DayThree/Increment.java
@@ -0,0 +1,9 @@
+package DayThree;
+
+public class Increment {
+    public static void main(String [] args){
+        for(int x = 1; x<10; x*=2){
+            System.out.println(x);
+        }
+    }
+}
