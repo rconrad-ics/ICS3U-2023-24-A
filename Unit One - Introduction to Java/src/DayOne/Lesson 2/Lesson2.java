4a0d79765a2759820c65b8813d3662253b1ae65b
Ross Conrad <rcontrad@bayviewglen.ca>
2023-09-28 08:59:52 -0400

    Homework #1 Sept 28, 2023

--- Unit One - Introduction to Java/src/DayOne/Lesson 2/Lesson2.java
+++ Unit One - Introduction to Java/src/DayOne/Lesson 2/Lesson2.java
@@ -3,6 +3,6 @@ package lesson2;
 public class ExampleOne{
     
     public static void main(String[] args){
-        System.out.println("Hello ICS3U!");
+        System.out.println("Hello ICS3U!This is a test");
     }
 }
