0b8a9c6b64a7fb42c7960bf6635a0f6b443eac44
Ross Conrad <rcontrad@bayviewglen.ca>
2023-10-02 08:51:15 -0400

    Pools question

--- Unit One - Introduction to Java/src/DayOne/Lesson 2/Lesson2.java
+++ Unit One - Introduction to Java/src/DayOne/Lesson 2/Lesson2.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 package lesson2;
 
-public class ExampleOne{
+
+public class ExampleOne(){
     
     public static void main(String[] args){
-        System.out.println("Hello ICS3U!This is a test");
+        System.out.println("PLease enter a number");
+        Scanner in = new Scanner(System.in);
+        System.out.println("The number entered was: " + in);
+
     }
 }
